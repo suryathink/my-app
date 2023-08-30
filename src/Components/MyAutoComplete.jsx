db5cec693c78f979a372b95c0893c231f10f307b
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-08-30 11:10:49 +0530

    l

---
+++ src/Components/MyAutoComplete.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import { Autocomplete, InputAdornment } from "@mui/material";
+import BasicSelect from "./Select";
+import "../App.css"
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+
+export default function ComboBox() {
+  return (
+    <Autocomplete
+      style={{ margin: "20px 20px 20px" }}
+      disablePortal
+      disableClearable
+      forcePopupIcon={false}
+      id="combo-box-demo"
+      options={top100Films}
+      sx={{ width: 600, maxHeight: 300 }}
+      renderInput={(params) => (
+        <TextField
+          // sx={{ width: 500 }}
+          {...params}
+          label="Movie"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <InputAdornment position="end">
+                {/* <AccountBalanceIcon /> */}
+                <BasicSelect />
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
+
+const top100Films = [
+  { label: "The Shawshank Redemption", year: 1994 },
+  { label: "The Godfather", year: 1972 },
+  { label: "The Godfather: Part II", year: 1974 },
+  { label: "The Dark Knight", year: 2008 },
+  { label: "12 Angry Men", year: 1957 },
+  { label: "Spirited Away", year: 2001 },
+  { label: "Saving Private Ryan", year: 1998 },
+  { label: "Once Upon a Time in the West", year: 1968 },
+  { label: "American History X", year: 1998 },
+  { label: "Interstellar", year: 2014 },
+  { label: "Casablanca", year: 1942 },
+  { label: "City Lights", year: 1931 },
+  { label: "Psycho", year: 1960 },
+  { label: "The Green Mile", year: 1999 },
+  { label: "The Intouchables", year: 2011 },
+  { label: "Modern Times", year: 1936 },
+  { label: "Raiders of the Lost Ark", year: 1981 },
+];
---
+++ src/Components/Select.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    // <div style={{ width: "80px", height: "50px", border: "2px solid red" }}>
+    <Box sx={{ minWidth: 80, maxWidth: 150, minHeight: 50, maxHeight: 250 }}>
+      <FormControl>
+        {/* <InputLabel id="demo-simple-select-label">Age</InputLabel> */}
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          //   label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={2}>Two</MenuItem>
+          <MenuItem value={1}>One</MenuItem>
+          <MenuItem value={6}>Six</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+    // </div>
+  );
+}
